dcac759be1637efcc7b3c00444fcdde1f8f29b23
Rudie Dirkx <github@hotblocks.nl>
2019-07-01 15:25:45 +0200

    +HasManyScalar

---
+++ src/HasManyScalar.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace rdx\aggrel;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+
+class HasManyScalar extends Relation {
+
+	protected $targetKey;
+	protected $foreignKey;
+	protected $localKey;
+
+	public function __construct(QueryBuilder $query, Model $parent, $targetKey, $foreignKey, $localKey = null) {
+		$this->query = $query;
+		$this->parent = $parent;
+		$this->targetKey = $targetKey;
+		$this->foreignKey = $foreignKey;
+		$this->localKey = $localKey ?: $parent->getKeyName();
+	}
+
+	/**
+	 * Set the constraints for a lazy load of the relation.
+	 */
+	public function addConstraints() {
+		$this->query->where($this->foreignKey, '=', $this->parent->getAttribute($this->localKey));
+	}
+
+	/**
+	 * Set the constraints for an eager load of the relation.
+	 */
+	public function addEagerConstraints(array $models) {
+		$this->query->whereIn($this->foreignKey, array_column($models, $this->localKey));
+		$this->select([$this->foreignKey, $this->targetKey]);
+	}
+
+	/**
+	 * Get the relationship for eager loading.
+	 */
+	public function getEager() {
+		return (new Collection($this->query->get()))->groupBy($this->foreignKey);
+	}
+
+	/**
+	 * Match the eagerly loaded results to their parents.
+	 */
+	public function match(array $models, Collection $results, $relation) {
+		foreach ($models as $model) {
+			$id = $model->getAttribute($this->localKey);
+			if (isset($results[$id])) {
+				$model->setRelation($relation, $results[$id]->pluck($this->targetKey, $this->targetKey)->all());
+			}
+		}
+
+		return $models;
+	}
+
+	/**
+	 * Get the results of the relationship.
+	 */
+	public function getResults() {
+		$this->addConstraints();
+		return $this->query->pluck($this->targetKey, $this->targetKey);
+	}
+
+	/**
+	 * Initialize the relation on a set of models.
+	 */
+	public function initRelation(array $models, $relation) {
+		foreach ($models as $model) {
+			$model->setRelation($relation, []);
+		}
+
+		return $models;
+	}
+
+}
--- src/RelatesToAggregates.php
+++ src/RelatesToAggregates.php
@@ -2,6 +2,8 @@
 
 namespace rdx\aggrel;
 
+use Illuminate\Database\Query\Builder as QueryBuilder;
+
 trait RelatesToAggregates {
 
 	protected function hasAggregate($related, $aggregate, $foreignKey, $localKey = null) {
@@ -16,6 +18,14 @@ trait RelatesToAggregates {
 	protected function hasCount($related, $foreignKey, $localKey = null) {
 		return $this->hasAggregate($related, 'count(1)', $foreignKey, $localKey)
 			->default(0);
+	}
+
+	protected function hasManyScalar($targetKey, $targetTable, $foreignKey, $localKey = null) {
+		$conn = $this->getConnection();
+		$grammar = $conn->getQueryGrammar();
+		$query = new QueryBuilder($conn, $grammar, $conn->getPostProcessor());
+
+		return new HasManyScalar($query->from($targetTable), $this, $targetKey, $foreignKey, $localKey);
 	}
 
 }
