9b9aadde33a8ecdb3eeac7181248ca14cc175a8b
Rudie Dirkx <github@hotblocks.nl>
2019-08-12 23:54:22 +0200

    +Expression key names

--- src/RelatesToAggregates.php
+++ src/RelatesToAggregates.php
@@ -3,15 +3,20 @@
 namespace rdx\aggrel;
 
 use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Query\Expression;
 
 trait RelatesToAggregates {
 
 	protected function hasAggregate($related, $aggregate, $foreignKey, $localKey = null) {
 		$instance = new $related();
 
-		$localKey = $localKey ?: $this->getKeyName();
+		$localKey or $localKey = $this->getKeyName();
 
-		return (new HasAggregate($instance->newQuery(), $this, $instance->getTable().'.'.$foreignKey, $localKey))
+		if (!($foreignKey instanceof Expression)) {
+			$foreignKey = $instance->getTable() . '.' . $foreignKey;
+		}
+
+		return (new HasAggregate($instance->newQuery(), $this, $foreignKey, $localKey))
 			->aggregate($aggregate);
 	}
 
