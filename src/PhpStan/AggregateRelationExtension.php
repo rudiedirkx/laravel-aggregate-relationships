19b53dee01b765c78657db0b275a55f9a421c2a7
Rudie Dirkx <github@hotblocks.nl>
2024-10-22 00:22:16 +0200

    phpstan support for HasManyScalar

--- src/PhpStan/AggregateRelationExtension.php
+++ src/PhpStan/AggregateRelationExtension.php
@@ -9,13 +9,16 @@ use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\ParametersAcceptorSelector;
 use PHPStan\Reflection\PropertiesClassReflectionExtension;
 use PHPStan\Reflection\PropertyReflection;
+use PHPStan\Type\ArrayType;
 use PHPStan\Type\FloatType;
 use PHPStan\Type\IntegerType;
 use PHPStan\Type\NeverType;
 use PHPStan\Type\NullType;
 use PHPStan\Type\ObjectType;
+use PHPStan\Type\StringType;
 use PHPStan\Type\UnionType;
 use rdx\aggrel\HasAggregateTable;
+use rdx\aggrel\HasManyScalar;
 
 /**
  * @see Larastan\Larastan\Properties\ModelRelationsExtension
@@ -38,19 +41,33 @@ class AggregateRelationExtension implements PropertiesClassReflectionExtension {
 		}
 
 		$returnType = ParametersAcceptorSelector::selectSingle($classReflection->getNativeMethod($propertyName)->getVariants())->getReturnType();
-		if (!(new ObjectType(HasAggregateTable::class))->isSuperTypeOf($returnType)->yes()) {
-			return false;
+		if (
+			(new ObjectType(HasAggregateTable::class))->isSuperTypeOf($returnType)->yes() ||
+			(new ObjectType(HasManyScalar::class))->isSuperTypeOf($returnType)->yes()
+		) {
+			return true;
 		}
 
-		return true;
+		return false;
 	}
 
 	public function getProperty(ClassReflection $classReflection, string $propertyName) : PropertyReflection {
-		$type = new UnionType([
-			new IntegerType(),
-			new FloatType(),
-			// new NullType(),
-		]);
+		$returnType = ParametersAcceptorSelector::selectSingle($classReflection->getNativeMethod($propertyName)->getVariants())->getReturnType();
+		if ((new ObjectType(HasAggregateTable::class))->isSuperTypeOf($returnType)->yes()) {
+			$type = new UnionType([
+				new IntegerType(),
+				new FloatType(),
+			]);
+		}
+		else {
+			$innerType = new UnionType([
+				new IntegerType(),
+				new FloatType(),
+				new StringType(),
+			]);
+			$type = new ArrayType(new IntegerType(), $innerType);
+		}
+
 		return new ModelProperty($classReflection, $type, new NeverType(), false);
 	}
 
